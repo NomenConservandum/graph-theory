0333258fc477e7ad7841788ea9dad362d3e6d5d1
NomenConservandum <hramov.sanya2018@yandex.ru>
2025-10-16 12:38:30 +0400

    Fix: task4

--- task1/task4.go
+++ task1/task4.go
@@ -2,6 +2,10 @@ package main
 
 func task4Func(graph *GraphInfo) []*Node {
 	var isIsolatedList = make(map[*Node]bool, len(graph.connectionsList))
+	for _, node := range graph.nodes {
+		isIsolatedList[node] = false
+	}
+
 	for _, edges := range graph.connectionsList {
 		for _, edge := range edges {
 			isIsolatedList[edge.List[1]] = true
